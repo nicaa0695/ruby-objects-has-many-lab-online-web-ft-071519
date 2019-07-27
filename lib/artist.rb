3bd2fd8d9e238975bae38ee009f45ee149715172
Veronica Lopez <nicaa0695@gmail.com>
2019-07-27 19:48:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,3 @@
-class Artist
-  attr_accessor :name, :artist
 
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    #@artist = artist
-    @songs = []
-  end
-
-  def songs
-    @songs
-  end
-
-  def artist
-    @artist
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-   @@song_count
-  end
-
-end
 
   
